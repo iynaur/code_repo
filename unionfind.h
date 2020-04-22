78ff2fd86ec8eeafde7460f3630c22288b72ce7a
ruanyi <iynaur87@sina.com>
2020-04-22 10:59:39 +0800

    Update unionfind.h

--- unionfind.h
+++ unionfind.h
@@ -44,3 +44,55 @@ struct UnionFind {//index [0, n)
     }
 
 };
+
+
+struct UnionFindWithCnt {//index [0, n)
+    vector<int> fa;
+    vector<int> cnt;
+    explicit UnionFind(int n){
+        fa.resize(n);
+        for (int i=0; i<n; ++i)fa[i]=i;
+
+        cnt = vector<int>(n, 1);
+    }
+
+    int findP(int i) {
+        while(i != fa[i]) {
+            fa[i] = fa[fa[i]];
+            i = fa[i];
+        }
+        return i;
+    }
+
+    int find(int n) {
+        int m = n;
+        while (fa[m]!=m)
+            m = fa[m];
+        fa[n] = m;
+        return m;
+    }
+    void unite(int m, int n) {// n not change
+        int p1 = find(m);
+        int p2 = find(n);
+        if (p1!=p2)
+        {
+          fa[p1] = p2;
+          cnt[p2] += cnt[p1];
+          cnt[p1]=0;
+        }
+        return;
+    }
+
+    int g(int son)
+    {
+        while (fa[fa[son]] != fa[son]) fa[son] = fa[fa[son]];
+        return fa[son];
+    }
+
+    void connectAtoB(int a, int b)// b not change
+    {
+        //if (a>b) swap(a, b);
+        unite(a, b);
+    }
+
+};
