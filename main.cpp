d817ae91111dee7006f3ff95bfbd1e1ab77d3a35
ruanyi <iynaur87@sina.com>
2020-05-07 17:04:55 +0800

    code

--- main.cpp
+++ main.cpp
@@ -14,81 +14,119 @@
 #include <chrono>
 #include <cstring>
 
-#include "segtree.h"
-#include "pairhash.h"
-#include "kmp.h"
-//#include "leetcode_bintree.h"
-#include "leetcode_list.h"
 using namespace std;
-
-struct ScopedNanoTimer
-{
-    chrono::high_resolution_clock::time_point t0;
-    string title;
-
-    ScopedNanoTimer(string title_)
-        : t0(chrono::high_resolution_clock::now())
-    {
-        title=title_;
-    }
-    ~ScopedNanoTimer(void)
-    {
-        auto  t1 = chrono::high_resolution_clock::now();
-        auto nanos = chrono::duration_cast<chrono::milliseconds>(t1-t0).count();
-
-        cout<<title<<" cost "<<nanos<<" ms\n";
-    }
-};
-
-using namespace std;
-
 typedef long long lint;
 typedef vector<int> vi;
 typedef pair<int,int> pii;
 
 typedef vector<lint> vl;
 typedef pair<lint,lint> pll;
+typedef unsigned long long ul;
+typedef vector<ul> vu;
 
-int main_()
+#define mpr make_pair
+
+class Solution {
+  int n;
+  vu s, t, u, v;
+
+  vu cu, cv;
+
+  vector<vu> a;
+
+  bool isRowSatis(int r){
+
+  }
+
+  void sol(int bid){
+    cu.resize(n); cv.resize(n);
+    ul vb = 1<<bid;
+    for (int i=0; i<n; ++i) cu[i] = u[i] & vb;
+    for (int i=0; i<n; ++i) cv[i] = v[i] & vb;
+
+    a.assign(n, vu(n, -1));
+
+    vi satisrow(n, 0);
+    vi satiscol(n, 0);
+
+
+    for (int i=0; i<n; ++i){
+      if (s[i] == 0 && cu[i] ||
+          s[i] == 1 && cu[i] == 0) satisrow[i] = 1;
+      for (int j = 0; j<n; ++j){
+        if (t[j] == 0 && cv[j] ||
+            t[j] == 1 && cv[j] == 0) satiscol[j] = 1;
+
+        if (s[i] == 0 && cu[i] ){//and
+          if (t[j] == 0 && cv[j] == 0){
+            cout<<-1; exit(0);
+          }
+          a[i][j] = 1;
+
+        }
+        else if (s[i] == 0 && cu[i] == 0){
+          if (cv[j] == 0) a[i][j] = 0;
+          else if (t[j] == 0) {
+
+            a[i][j] = 1;
+
+          }
+          satiscol[j] = 1;
+          satisrow[i] = 1;
+        }
+        else if (s[i] == 1 && cu[i]){//or
+          if (cv[i]){
+            a[i][j]=1;
+          }
+          else if (t[i]) {
+
+            a[i][j]=0;
+          }
+          satiscol[j] = 1;
+          satisrow[i] = 1;
+        }
+        else {
+          if (t[j] && cv[j]){
+            cout<<-1; exit(0);
+          }
+          a[i][j] = 0;
+        }
+      }
+    }
+
+    set<int> needrow, needcol;
+    for (int i=0; i<n; ++i){
+      if (!satisrow[i]) {
+
+        needrow.insert(i);
+      }
+      if (!satiscol[i]) needcol.insert(i);
+    }
+
+    if (needrow.size() == 1 || needcol.size() == 1){cout<<-1; exit(0);}
+
+//    if (needrow.size() == 0 || needcol.size() == 0)
+  }
+
+public:
+  void sol(){
+    cin>>n;
+   s.resize(n);
+   t.resize(n);
+   u.resize(n);
+   v.resize(n);
+   for (ul &v: s) cin>>v;
+   for (ul &v: t) cin>>v;
+   for (ul &v: u) cin>>v;
+   for (ul &v_: v) cin>>v_;
+
+
+  }
+};
+
+int main()
 {
     ios_base::sync_with_stdio(0);    cin.tie(NULL);    cout.tie(NULL);
-
-    lint n,l; cin>>n>>l;
-    vector<pll> ab(n);
-    lint minmin = 1e9;
-    lint maxmax = 0;
-    for (int i=0;i<n; ++i) {
-        cin>>ab[i].first>>ab[i].second;
-        minmin = min(minmin, ab[i].first);
-        maxmax = max(maxmax, ab[i].second);
-    }
-
-    sort(ab.begin(), ab.end(), [](pll a, pll b)
-         {
-             return a.second < b.second;
-         });
-
-    vector<lint> sum(n+1, 0);
-    for (int i=1; i<=n; ++i) sum[i] = sum[i-1] + ab[i-1].second;
-
-    lint maxmin = l/n;
-
-    //SegmentTree<lint> segb();
-    auto checkmax = [&](lint m)
-    {
-        int id = upper_bound(ab.begin(), ab.end(), make_pair(m,m), [](pll a, pll b)
-                              {
-                                  return a.second < b.second;
-                              }) - ab.begin();
-    };
-
-
-    for (int rmin = minmin; rmin <= maxmin; ++rmin)
-    {
-
-    }
-
-
 
 
 
